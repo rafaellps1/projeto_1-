1988627de97be1f2c8d297946ee06f590faafc03
Rafael Rocha <rafael.lps.lr@gmail.com>
2023-10-31 12:07:05 -0300

    :tada2: initial commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function toggleMode() {
     //substituir a imagem
     if (html.classList.contains('light')) {
     //se tiver light mode adicionar a imagem light
-    img.setAttribute("src", "./assets/assests/avatar-light.png")
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
     } else {
     //se tiver sem light mode, manter a mesma imagem
     img.setAttribute("src", "./assets/assets/avatar.png")
